093ef2e6866beaba3cd8e6c9d4e441890855100f
Mark Pahor <mark@Marks-MBP.socal.rr.com>
2019-09-08 21:55:39 -0700

    Instructions container added

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from "./components/header";
+import Instructions from "./components/instructions"
 
 
 function App() {
   return (
-    <Header />
+    [<Header />, <Instructions />]
   );
 }
 
